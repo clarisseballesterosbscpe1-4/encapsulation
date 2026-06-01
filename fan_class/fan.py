c885d42b7fb636f961f82467dff43f5dd5556968
clarisseballesterosbscpe1-4 <clarisseballesteros08@gmail.com>
2026-06-02 07:39:55 +0800

    fixed some errors in my constants and make setters and getters

--- fan_class/fan.py
+++ fan_class/fan.py
@@ -2,9 +2,29 @@ class Fan:
     slow = 1
     medium = 2
     fast = 3
-    def __init__(self, speed=SLOW, radius=5, color="Blue", on=FALSE):
+
+    def __init__(self, speed=slow, radius=5, color="Blue", on=False):
+        self.__speed = speed
         self.speed = speed
         self.radius = radius
         self.color = color
         self.on = on
-    
+
+    def set_speed(self, speed):
+        self.speed = speed
+    def set_radius(self, radius):
+        self.radius = radius
+    def set_color(self, color):
+        self.color = color
+    def set_on(self, on):
+        self.on = on
+
+    def get_speed(self):
+        return self.speed
+    def get_radius(self):
+        return self.radius
+    def get_color(self):
+        return self.color
+    def get_on(self):
+        return self.on
+
