01f22ee91a318ea846328ed9aa289ed5c6a75a14
clarisseballesterosbscpe1-4 <clarisseballesteros08@gmail.com>
2026-06-02 07:51:29 +0800

    remove some added speed in my constants

--- fan_class/fan.py
+++ fan_class/fan.py
@@ -4,7 +4,6 @@ class Fan:
     fast = 3
 
     def __init__(self, speed=slow, radius=5, color="Blue", on=False):
-        self.__speed = speed
         self.speed = speed
         self.radius = radius
         self.color = color
