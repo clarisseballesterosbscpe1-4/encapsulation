04b16b0cefb5f5b1336f3a08735c8dc2ccb8d7bf
clarisseballesterosbscpe1-4 <clarisseballesteros08@gmail.com>
2026-06-01 17:08:43 +0800

    display program

--- pet_class/pet.py
+++ pet_class/pet.py
@@ -21,3 +21,22 @@ class Pet:
 
     def get_age(self):
         return self.__age
+
+my_pet = Pet()
+
+name = input("Enter pet name: ")
+animal_type = input("Enter pet animal type: ")
+age_number = input("Enter age number: ")
+age_unit = input("Enter unit (month/months/year/years): ")
+pet_age = f"{age_number} {age_unit} old"
+
+my_pet.set_name(name)
+my_pet.set_animal_type(animal_type)
+my_pet.set_age(pet_age)
+
+print("\nPet Information")
+print("---------------")
+print("Name:", my_pet.get_name())
+print("Animal Type:", my_pet.get_animal_type())
+print("Age:", my_pet.get_age())
+print("---------------")
