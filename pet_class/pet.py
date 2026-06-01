08d86458cddcc8b1fbcd406a56b219b437b3df90
clarisseballesterosbscpe1-4 <clarisseballesteros08@gmail.com>
2026-06-01 16:18:27 +0800

    class and accessors

---
+++ pet_class/pet.py
@@ -0,0 +1,15 @@
+class Pet:
+    def __init__self(self):
+        self.name = ""
+        self.__animal_type = ""
+        self.age = 0
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    def set_age(self, age):
+        self.__age = age
+    
